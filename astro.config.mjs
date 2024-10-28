3d34a55e8c20145681b15472dca9d50c9ef3d315
Jesus Pinar <not@email.com>
2024-10-28 22:15:38 +0100

    Update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ecoatasesores.com/",
+  site: "https://www.ecoatasesores.com/",
   base: "/ecoat-web",
   integrations: [
     tailwind(),
