cfab67df168bc12cd7fe4cd04eee1165a36f243c
Jesus Pinar <not@email.com>
2024-11-12 17:22:16 +0100

    Add ico

--- src/config/config.ts
+++ src/config/config.ts
@@ -27,7 +27,7 @@ export const configData: Config = {
 		'ECOAT es una asesoría situada en Denia especializada en áreas: contable, fiscal, laboral y mercantil.',
 	ogImage: '/og.jpg',
 	logo: {
-		src: '/logo.svg',
+		src: '/logo.ico',
 		alt: 'ECOAT. logo',
 	},
 	canonical: true,
